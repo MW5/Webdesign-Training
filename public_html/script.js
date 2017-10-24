fef306a28b8dc745fc0fd6133e09945b1bbc5735
MW5 <mwojcik1988@gmail.com>
2017-10-25 00:38:04 +0200

    added leaf and typewriter animations

--- public_html/script.js
+++ public_html/script.js
@@ -13,28 +13,34 @@ $(document).ready(function(){
         }
     });
 
-    //nav color change and carousel fade on carousel scroll
+    //typewriter effect on about, nav color change, carousel fade, on  scroll
     var colorChanged = false;
     var carouselShown = false;
+    var aboutTypewrited = false;
     var navColorChangeOffset = 75;
     var lastScrollTop = 0;
     $(window).scroll(function() {
         var st = $(this).scrollTop();
         if (st > lastScrollTop){
+            //typewriter on about
+            if ($(window).scrollTop()>=$("#about_section").position().top-$("#about_section").height()/2
+                    && aboutTypewrited==false) {
+                aboutTypewrited = true;
+                $("#about_text").css("display", "inline-block");
+                startTypewriting("#about_text", 100, 6000);
+            }
             //going down carousel
             if ($(window).scrollTop()>=$("#carousel_section").position().top-$("#carousel_section").height()
                     && $(window).scrollTop()<=$("#carousel_section").position().top
                     && carouselShown==false) {
                 $("#carousel_section").toggleClass("visible");
                 carouselShown = true;
-                console.log("1");
             }
             if ($(window).scrollTop()>=$("#carousel_section").position().top+$("#carousel_section").height()
                     && $(window).scrollTop()>=$("#carousel_section").position().top 
                     && carouselShown==true) {
                 $("#carousel_section").toggleClass("visible");
                 carouselShown = false;
-                console.log("2");
             }
             //going down nav
             if ($(window).scrollTop()>=$("#carousel_section").position().top-navColorChangeOffset
@@ -42,21 +48,18 @@ $(document).ready(function(){
                     && colorChanged==false) {
                 $(".nav-link").toggleClass("visible");
                 colorChanged = true;
-                console.log("11");
             }
             if ($(window).scrollTop()>=$("#carousel_section").position().top+$("#carousel_section").height()
                     && $(window).scrollTop()>=$("#carousel_section").position().top
                     && colorChanged==true) {
                 $(".nav-link").toggleClass("visible");
                 colorChanged = false;
-                console.log("22");
             }
         } else {
             //going up carousel
             if ($(window).scrollTop()<=$("#carousel_section").position().top+$("#carousel_section").height()
                     && $(window).scrollTop()>=$("#carousel_section").position().top
                     && carouselShown==false) {
-                console.log("3");
                 $("#carousel_section").toggleClass("visible");
                 carouselShown = true;
             }
@@ -65,23 +68,20 @@ $(document).ready(function(){
                     && carouselShown==true) {
                 $("#carousel_section").toggleClass("visible");
                 carouselShown = false;
-                console.log("4");
             }
-//            
+            
             //going up nav
             if ($(window).scrollTop()<=$("#carousel_section").position().top+$("#carousel_section").height()+navColorChangeOffset
                     && $(window).scrollTop()>=$("#carousel_section").position().top
                     && colorChanged==false) {
                 $(".nav-link").toggleClass("visible");
                 colorChanged = true;
-                console.log("33");
             }
            if ($(window).scrollTop()<=$("#carousel_section").position().top-navColorChangeOffset
                    && $(window).scrollTop()<=$("#carousel_section").position().top+$("#carousel_section").height()
                    && colorChanged==true) {
                $(".nav-link").toggleClass("visible");
                 colorChanged = false;
-                console.log("44");
             }
         }
         lastScrollTop = st;
@@ -92,4 +92,50 @@ $(document).ready(function(){
         target: '#nav_links',
         offset: 400
     });
+    
+        //calculate font size according to screen and desired ratio
+    function calcFontSize(ratio) {
+        var properRatio = ratio;
+        var properFontSize = $(document).width()/properRatio;
+        return properFontSize;
+    }
+    
+    //heading text ratio = 7.14
+    $("#heading_text").css("font-size",calcFontSize(7.14));
+    //about text ratio = 44.625
+    $("#about_text").css("font-size",calcFontSize(44.625));
+    
+    //start animations
+        //heading typewriting
+    setTimeout(function(){startTypewriting("#heading_text", 300, 3000);}, 3000);
+        //floating leaf animation 
+    setTimeout(function(){$("svg").addClass("moved");}, 3000);
+    
+    function startTypewriting(id, speed, fadeOffset) {
+            //get initial value
+        var initialText = $(id).text();
+        var typewritten = "";
+        //clean before typewriting
+        $(id).text(typewritten);
+        $(id).fadeIn(fadeOffset);
+
+        //set interval between letters
+        var interval = setInterval(function() {
+            typewrite();
+            },speed
+        );
+
+        var i =0;
+        function typewrite() {
+            if(i<initialText.length) {
+                typewritten += initialText.charAt(i);
+                $(id).text(typewritten);
+
+            } else {
+                clearInterval(interval);
+            }
+            i++;
+        }
+    }
+
 });
